9374cbb885c71a34646988857fb16b092b1414aa
叫我小朋友 <alexchan1st@163.com>
2019-11-19 02:07:02 +1100

    feature selection

--- feature_selection.py
+++ feature_selection.py
@@ -1,6 +1,6 @@
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split, StratifiedKFold,cross_val_score
+from sklearn.model_selection import train_test_split, StratifiedKFold, cross_val_score
 from sklearn.decomposition import PCA
 import numpy, pandas
 import get_dataset
@@ -20,30 +20,30 @@ def multivariate_pearsonr(X, y):
     return (numpy.array(scores), numpy.array(pvalues))
 
 
-def feature_select(x, y,labels,y_contin):
-
+def feature_select(x, y, labels, y_contin):
     scaling = StandardScaler()
     scaling.fit(x)
     x = scaling.transform(x)
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.10)
 
     # normalizing
-    #print(x)
+    # print(x)
 
     forest = RandomForestClassifier(n_estimators=10000, random_state=0, n_jobs=-1,
-                                  max_depth=15)
-    forest.fit(x_train, y_train)
+                                    max_depth=15)
+    forest.fit(x, y)
     print("__________Evaluating by RandomForest importance and pearson correlation__________")
-    print("1) rank , 2) Feature name , 3) RandomForest importance , 4) Pearson Correlation , 5) p_value")
-    pearson_score,p_values=multivariate_pearsonr(x,y_contin)
+    print(
+        "1) rank , 2) Feature name , 3) RandomForest importance , "
+        "4) Pearson Correlation , 5) p_value of pearson correlation")
+    pearson_score, p_values = multivariate_pearsonr(x, y_contin)
     importances = forest.feature_importances_
     indices = numpy.argsort(importances)[::-1]
     for f in range(x_train.shape[1]):
-        print("%2d) %-*s %f,%f,%f" % (f + 1, 30, labels[indices[f]], importances[indices[f]],pearson_score[indices[f]],
-                                      p_values[indices[f]]))
-    print("score:", forest.score(x_test,y_test))
-
-
+        print("%2d)         %-*s %-*f %-*f %-*f" % (
+        f + 1, 30, labels[indices[f]], 28, importances[indices[f]], 28, pearson_score[indices[f]],
+        28, p_values[indices[f]]))
+    print("score:", forest.score(x_test, y_test))
 
 
 if __name__ == "__main__":
@@ -52,15 +52,15 @@ if __name__ == "__main__":
     results = {}
     for label in dfs:
         df = dfs[label]
-        print("Evaluating features of "+label)
+        print("Evaluating features of " + label)
         # transfer the output values from Numeric to Class (0-> LOW , 1-> HIGH) using median
         y_median = df[label].median()
-        y_contin=df[label]
+        y_contin = df[label]
         df.loc[df[label] <= y_median, label] = 0
         df.loc[df[label] > y_median, label] = 1
         x = df.drop(columns=label)
-        print(x.shape)
-        labels=x.columns
+        #print(x.shape)
+        labels = x.columns
 
         y = df[label].to_numpy()
         # k fold cross validation
@@ -68,4 +68,4 @@ if __name__ == "__main__":
 
         accuracies = []
 
-        feature_select(x,y,labels,y_contin)
+        feature_select(x, y, labels, y_contin)
